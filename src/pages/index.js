0f0d8e91ee19b6985dd81166d2e5fe9cfeed1aba
Diego Bernal <diego91bernal@gmail.com>
2020-04-08 20:42:37 -0700

    typo in site description

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Loader from "../components/Loader";
 import SocialSharingButtons from "../components/SocialSharingButtons";
 
 export const PAGE_DESCRIPTION =
-  "Get the latest Coronavirus (COVID-19) updates in the Coachella Valley";
+  "Get the latest Coronavirus (COVID-19) updates for the Coachella Valley";
 export const PAGE_TITLE = "COVID-19 Updates for the Coachella Valley";
 const PAGE_IMAGE = "https://covid19cv.info/og-image.png";
 
